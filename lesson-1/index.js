f434651b4f110a083ed729b8db015c1a253bd450
abdullox0900 <abdullox0900@gmail.com>
2023-06-19 23:11:01 +0500

    feat: day-2 lesson-2

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,4 +1,5 @@
-'use strict'
+
+// 'use strict'
 
 // function Do() {
 //     let x = 0
@@ -14,15 +15,15 @@
 
 // console.log(res);
 
-let name = 'Ali';
-let age = 19
+// let name = 'Ali';
+// let age = 19
 
 // console.log('Ismi:', name, 'Yoshi:', age);
 
-let elBody = document.body
+// let elBody = document.body
 
-let elText = document.createElement('p').textContent = 'Hello';
+// let elText = document.createElement('p').textContent = 'Hello';
 
-elBody.append(elText)
+// elBody.append(elText)
 
-console.log(document.getElementById('text').textContent = `ismi: ${name} yoshi: ${age}`);
+// console.log(document.getElementById('text').textContent = `ismi: ${name} yoshi: ${age}`);
