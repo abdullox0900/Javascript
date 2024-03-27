0a01c64cd1c1efbedb89af4e5f91f9fb61102001
abdullox0900 <abdullox0900@gmail.com>
2024-03-27 21:30:08 +0500

    add js for

---
+++ JS-for/app.js
@@ -0,0 +1,11 @@
+let arr = new Array(100)
+
+let users = ['Abdulloh', 'Abdulaziz', 'Muhammad', 'Jasurbek']
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(i)
+}
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i])
+}
